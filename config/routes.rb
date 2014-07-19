d51a1550c74301d51c4b1322df69d72958003e0d
shimurausk <shimurausk@gmail.com>
2014-07-20 02:10:38 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
   #get "users/new"
-  resources :users
-  resources :sessions,   only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root 'static_pages#home'
   match '/signup',    to: 'users#new',    via: 'get'
   match '/signin',    to: 'sessions#new',    via: 'get'
@@ -10,7 +15,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  resources :users
+  #resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
